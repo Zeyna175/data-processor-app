df814d6d4fc7483a830fba7939e3f5372c71639c
Seynabou Gueye <naboug239@gmail.com>
2026-02-24 11:05:41 +0000

    Fix authentication headers in data processor service

--- python_Angular/src/app/services/data-processor.service.ts
+++ python_Angular/src/app/services/data-processor.service.ts
@@ -1,27 +1,37 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataProcessorService {
-  private apiUrl = '/api';
+  private apiUrl = 'https://data-processor-app.onrender.com/api';
 
   constructor(private http: HttpClient) { }
+
+  private getHeaders(): HttpHeaders {
+    const token = localStorage.getItem('token');
+    return new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
+  }
 
   analyzeFile(file: File): Observable<any> {
     const formData = new FormData();
     formData.append('file', file);
-    return this.http.post(`${this.apiUrl}/analyze`, formData);
+    const headers = this.getHeaders();
+    return this.http.post(`${this.apiUrl}/analyze`, formData, { headers });
   }
 
   processFile(filename: string, options: any): Observable<any> {
-    return this.http.post(`${this.apiUrl}/process`, { filename, options });
+    const headers = this.getHeaders();
+    return this.http.post(`${this.apiUrl}/process`, { filename, options }, { headers });
   }
 
   downloadFile(filename: string): Observable<Blob> {
-    return this.http.get(`${this.apiUrl}/download/${filename}`, { responseType: 'blob' });
+    const headers = this.getHeaders();
+    return this.http.get(`${this.apiUrl}/download/${filename}`, { headers, responseType: 'blob' });
   }
 
   getStatus(): Observable<any> {
